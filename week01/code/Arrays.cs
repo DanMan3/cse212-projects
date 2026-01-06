a60ad1d261df744bdde5dcb525cd4aa11f5d432a
DanMan3 <dhbennion@gmail.com>
2026-01-05 20:39:12 -0700

    Week 02 Learning Activities completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -46,10 +46,5 @@ public static class Arrays
         listData.RemoveRange(listData.Count - amount, amount);
         listData.InsertRange(0, slice);
 
-
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
     }
 }
