2e32bf86445f26d92648326e89818bbe2a658dc7
DanMan3 <dhbennion@gmail.com>
2026-01-06 16:57:53 -0700

    Week 02 Queues assignment completed.

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,155 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with three PriorityItems of different values: Diamond (10), $5 bill (4),
+    // and Lawnmower blade (6). Enqueue them in the following order: blade, diamond, dollar.
+    // Ensure that the queue is in that order.
+    // Expected Result: "Lawnmower blade", "Diamond", "$5 bill"
+    // Defect(s) Found: None found. Everything runs properly.
     public void TestPriorityQueue_1()
     {
+        var diamond = new PriorityItem("Diamond", 10);
+        var dollar = new PriorityItem("$5 bill", 4);
+        var blade = new PriorityItem("Lawnmower blade", 6);
+
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(blade.Value, blade.Priority);
+        priorityQueue.Enqueue(diamond.Value, diamond.Priority);
+        priorityQueue.Enqueue(dollar.Value, dollar.Priority);
+
+
+        string[] expectedResult = new[] { "Lawnmower blade", "Diamond", "$5 bill" };
+
+        int queueLength = priorityQueue.GetQueueLength();
+        while (queueLength > 0)
+        {
+            if (queueLength > expectedResult.Length)
+            {
+                Assert.Fail("Queue should have ran out of items by now.");
+            }
+
+            var items = priorityQueue.GetQueueStrings();
+            CollectionAssert.AreEqual(expectedResult, items);
+            queueLength--;
+        }
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with three PriorityItems of different values: Diamond (10), $5 bill (4),
+    // and Lawnmower blade (6). Enqueue them in the following order: blade, diamond, dollar.
+    // Ensure that the queue after the most valuable item is taken out removes the most valuable item.
+    // Expected Result: "Lawnmower blade", "$5 bill"
+    // Defect(s) Found: Dequeue does not remove the highest value PriorityItem from the queue. Fixed by
+    // ensuring that it removes.
     public void TestPriorityQueue_2()
     {
+        var diamond = new PriorityItem("Diamond", 10);
+        var dollar = new PriorityItem("$5 bill", 4);
+        var blade = new PriorityItem("Lawnmower blade", 6);
+
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(blade.Value, blade.Priority);
+        priorityQueue.Enqueue(diamond.Value, diamond.Priority);
+        priorityQueue.Enqueue(dollar.Value, dollar.Priority);
+
+        priorityQueue.Dequeue();
+
+
+        string[] expectedResult = new[] { "Lawnmower blade", "$5 bill" };
+
+        int queueLength = priorityQueue.GetQueueLength();
+        while (queueLength > 0)
+        {
+
+            var items = priorityQueue.GetQueueStrings();
+            CollectionAssert.AreEqual(expectedResult, items);
+            queueLength--;
+        }
+
     }
 
-    // Add more test cases as needed below.
+
+
+
+    [TestMethod]
+    // Scenario: Create a queue with PriorityItems of different values, and at two of them have the same value.
+    // Enqueue each item, then run Dequeue. Compare the expected result to the highest priority item that is
+    // closer to the front of the queue.
+    // Expected Result: "Diamond"
+    // Defect(s) The Dequeue tie breaking (>=) was wrong. It selected the item further to the back compared to the front.
+    // set the tie breaking to be greater than (>) instead of (=).
+    public void TestPriorityQueue_3()
+    {
+        var diamond = new PriorityItem("Diamond", 10);
+        var dollar = new PriorityItem("$5 bill", 4);
+        var blade = new PriorityItem("Lawnmower blade", 6);
+        var tire = new PriorityItem("Tire", 10);
+        var pumpkin = new PriorityItem("Pumpkin", 2);
+        var car = new PriorityItem("Car", 9);
+        var paper = new PriorityItem("Piece of Paper", 1);
+
+
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(blade.Value, blade.Priority);
+        priorityQueue.Enqueue(diamond.Value, diamond.Priority);
+        priorityQueue.Enqueue(tire.Value, tire.Priority);
+        priorityQueue.Enqueue(dollar.Value, dollar.Priority);
+        priorityQueue.Enqueue(pumpkin.Value, pumpkin.Priority);
+        priorityQueue.Enqueue(car.Value, car.Priority);
+        priorityQueue.Enqueue(paper.Value, paper.Priority);
+
+
+        string expectedResult = "Diamond";
+        var actualResult = priorityQueue.Dequeue();
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+    [TestMethod]
+    // Scenario: Create a queue with PriorityItems of different values, and at two of them have the same value.
+    // Enqueue each item, then run Dequeue. Ensure that the highest value item can be at the back and it is still counted.
+    // Expected Result: "Diamond"
+    // Defect(s) In the Dequeue function, the for loop stops before the last element, meaning that the code never
+    // compares the final item's priority. removed the "- 1" in the for loop.
+    public void TestPriorityQueue_4()
+    {
+        var dollar = new PriorityItem("$5 bill", 4);
+        var blade = new PriorityItem("Lawnmower blade", 6);
+        var pumpkin = new PriorityItem("Pumpkin", 2);
+        var car = new PriorityItem("Car", 9);
+        var paper = new PriorityItem("Piece of Paper", 1);
+        var diamond = new PriorityItem("Diamond", 10);
+
+
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(blade.Value, blade.Priority);
+        priorityQueue.Enqueue(dollar.Value, dollar.Priority);
+        priorityQueue.Enqueue(pumpkin.Value, pumpkin.Priority);
+        priorityQueue.Enqueue(car.Value, car.Priority);
+        priorityQueue.Enqueue(paper.Value, paper.Priority);
+        priorityQueue.Enqueue(diamond.Value, diamond.Priority);
+
+
+        string expectedResult = "Diamond";
+        var actualResult = priorityQueue.Dequeue();
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    // Scenario: Create an instance of PriorityQueue and try to Dequeue without ever calling Enqueue. Assert a ThrowsException
+    // and ensure that it is throwing the right kind of exception and that the exception says "the queue is empty."
+    // Expected Result: ("") -- an empty string.
+    // Defect(s) Found: None. 
+    public void TestPriorityQueue_5()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        var msg = Assert.ThrowsException<InvalidOperationException>(() => priorityQueue.Dequeue());
+        Assert.AreEqual("The queue is empty.", msg.Message);
+
+    }
+
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,14 +11,15 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: The AreEqual check failed on the first attempt because the expected name is Bob, but Sue is given.
+    // This is because the Enqueue function was always inserting the new person at index 0. Fixed by changing to the Add method instead of insert.
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
         var tim = new Person("Tim", 5);
         var sue = new Person("Sue", 3);
 
-        Person[] expectedResult = [bob, tim, sue, bob, tim, sue, tim, sue, tim, tim];
+        Person[] expectedResult = new[] { bob, tim, sue, bob, tim, sue, tim, sue, tim, tim };
 
         var players = new TakingTurnsQueue();
         players.AddPerson(bob.Name, bob.Turns);
@@ -43,7 +44,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: This one appears to run correctly after fixing the issue with the last test above. Results are correct.
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -51,7 +52,7 @@ public class TakingTurnsQueueTests
         var sue = new Person("Sue", 3);
         var george = new Person("George", 3);
 
-        Person[] expectedResult = [bob, tim, sue, bob, tim, sue, tim, george, sue, tim, george, tim, george];
+        Person[] expectedResult = new[] { bob, tim, sue, bob, tim, sue, tim, george, sue, tim, george, tim, george };
 
         var players = new TakingTurnsQueue();
         players.AddPerson(bob.Name, bob.Turns);
@@ -85,7 +86,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: People with infinite turns (Tim) were not being enqueued again. Solved by adding an else if 
+    // to make sure that people with infinite turns (0) get enqueued again.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -94,7 +96,7 @@ public class TakingTurnsQueueTests
         var tim = new Person("Tim", timTurns);
         var sue = new Person("Sue", 3);
 
-        Person[] expectedResult = [bob, tim, sue, bob, tim, sue, tim, sue, tim, tim];
+        Person[] expectedResult = new[] { bob, tim, sue, bob, tim, sue, tim, sue, tim, tim };
 
         var players = new TakingTurnsQueue();
         players.AddPerson(bob.Name, bob.Turns);
@@ -116,14 +118,15 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Negative numbers were not recognized by the enqueue. From the last problem, 
+    // the else if checks if person.Turns == 0. Modified so that if person.Turns <= 0 it still works.
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
         var tim = new Person("Tim", timTurns);
         var sue = new Person("Sue", 3);
 
-        Person[] expectedResult = [tim, sue, tim, sue, tim, sue, tim, tim, tim, tim];
+        Person[] expectedResult = new[] { tim, sue, tim, sue, tim, sue, tim, tim, tim, tim };
 
         var players = new TakingTurnsQueue();
         players.AddPerson(tim.Name, tim.Turns);
@@ -143,7 +146,8 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: Nothing found. Everything seems to run as it should. The InvalidOperationException
+    // is thrown properly.
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
